e36ecbd48d0e65616b9ad3b9024a1d02da5b9553
Shun <153587021+buglover23@users.noreply.github.com>
2024-03-13 16:10:42 +0900

    ccreate pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,21 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import SignIn from "./pages/SignIn"
+import SingUp from "./pages/SingUp"
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
 export default function App() {
   return (
-    <div className="text-3xl text-red-500">App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/sign-in" element={<SignIn/>}/>
+      <Route path="/sign-up" element={<SingUp/>}/>
+      <Route path="/dashboard" element={<Dashboard/>}/>
+      <Route path="/projects" element={<Projects/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
