1469bae56209cc4bb6fc82c4077c34e2d0c24414
Shun <153587021+buglover23@users.noreply.github.com>
2024-04-07 12:07:11 +0900

    complete create post function

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -6,12 +6,15 @@ import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/
 import { app } from '../firebase'
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css'
+import { useNavigate } from "react-router-dom";
 export default function CreatePost() {
 
     const [file, setFile] = useState(null);
     const [imageUploadProgress, setImageUploadProgress] = useState(null);
     const [imageUploadError, setImageUploadError] = useState(null);
     const [formData, setFormData] = useState({});
+    const [publishError, setPublishError] = useState(null);
+    const navigate = useNavigate();
     const handleUploadImage = async () => {
         try {
             if (!file) {
@@ -47,13 +50,48 @@ export default function CreatePost() {
         }
     }
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await fetch('/api/post/create', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            });
+            const data = await res.json();
+
+            if (!res.ok) {
+                setPublishError(null);
+                return;
+            }
+
+            if(res.ok) {
+                setPublishError(null);
+                navigate(`/post/${data.slug}`);
+            }
+        } catch (error) {
+            setPublishError(error.message);
+        }
+    }
+
     return (
         <div className="p-3 max-w-3xl mx-auto min-h-screen">
             <h1 className="text-center text-3xl my-7 font-semibold">Create a post</h1>
-            <form className="flex flex-col gap-4">
+            <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
                 <div className="flex flex-col gap-4 sm:flex-row justify-between">
-                    <TextInput type="text" placeholder="Title" required id="title" className="flex-1" />
-                    <Select>
+                    <TextInput type="text" placeholder="Title" required id="title" className="flex-1"
+                        onChange={(e) => {
+                            setFormData({ ...formData, title: e.target.value })
+                        }}
+                    />
+                    <Select
+                        onChange={(e) => {
+                            setFormData({ ...formData, category: e.target.value })
+                        }}
+                    >
+
                         <option value="uncategorized">Select a category</option>
                         <option value="javascript">Javascript</option>
                         <option value="reactjs">React.js</option>
@@ -67,7 +105,7 @@ export default function CreatePost() {
                 <div className="flex gap-4 items-center justify-between border-2 border-red-400 border-dotted p-3">
                     <FileInput type='file' accept='image/*' onChange={(e) => setFile(e.target.files[0])} />
                     <Button type="button" gradientDuoTone='pinkToOrange' size='sm' outline onClick={handleUploadImage}
-                    disabled={imageUploadProgress}
+                        disabled={imageUploadProgress}
                     >
                         {
                             imageUploadProgress ? (<div className="w-16 h-16">
@@ -81,7 +119,7 @@ export default function CreatePost() {
                 </div>
                 {
                     imageUploadError && (
-                        <Alert color = 'failure'>
+                        <Alert color='failure'>
                             <span>{imageUploadError}</span>
                         </Alert>
                     )
@@ -89,13 +127,24 @@ export default function CreatePost() {
 
                 {
                     formData.image && (
-                        <img src={formData.image} alt="upload" className="w-full h-72 object-cover"/>
+                        <img src={formData.image} alt="upload" className="w-full h-72 object-cover" />
                     )
                 }
-                <ReactQuill theme="snow" className="h-72 mb-12" required />
+                <ReactQuill theme="snow" className="h-72 mb-12" required onChange={
+                    (value) => {
+                        setFormData({ ...formData, content: value })
+                    }
+                } />
                 <Button type="submit" gradientDuoTone='pinkToOrange' size='lg'>
                     Publish
                 </Button>
+                {
+                    publishError && (
+                        <Alert mt-5 color='failure'>
+                            <span>{publishError}</span>
+                        </Alert>
+                    )
+                }
             </form>
         </div>
     )
